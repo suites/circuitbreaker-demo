f10c9034a134ba4aaedea5513155737790108f9e
fred <woosiks.io@gmail.com>
2023-03-16 15:25:15 +0900

    feat: init Project

---
+++ src/main/kotlin/me/suitee/circuitbreakerdemo/CircuitbreakerDemoApplication.kt
@@ -0,0 +1,11 @@
+package me.suitee.circuitbreakerdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CircuitbreakerDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CircuitbreakerDemoApplication>(*args)
+}
---
+++ src/test/kotlin/me/suitee/circuitbreakerdemo/CircuitbreakerDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.suitee.circuitbreakerdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircuitbreakerDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
